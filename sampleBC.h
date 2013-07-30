555e46331deac54cca63bc59405f000f5c9b60ce
Bryan Thompson <bryan@systap.com>
2013-07-30 18:25:04 +0000

    Added license header and guards against repeated include of the sampleBC.h file.

--- sampleBC.h
+++ sampleBC.h
@@ -1,9 +1,32 @@
+/*********************************************************
+
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+
+**********************************************************/
 /* 
  * File:   adaptiveBC.h
  * Author: Zhisong Fu
  *
  * Created on July 9, 2013, 9:49 AM
  */
+
+#ifndef SAMPLEBC_H
+#define	SAMPLEBC_H
 
 template<int HOST>
 __host__ __device__ int
@@ -231,4 +254,5 @@ void accum_BC(thrust::device_vector<double>& d_BC, thrust::device_vector<adaptiv
 #endif
 }
 
+#endif	/* SAMPLEBC_H */
 
