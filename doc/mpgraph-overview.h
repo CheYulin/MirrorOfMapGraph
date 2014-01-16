db69c2bd3942c29b25abfd2f88252b9f80726046
Bryan Thompson <bryan@systap.com>
2014-01-16 21:33:29 +0000

    More work on the user documentation.  There are now graphics for the frontier, topology, and user data.  I have also included a PDF of the worksheet containing the data layout and the API overview and the worksheet itself which was the source for these data.

--- doc/mpgraph-overview.h
+++ doc/mpgraph-overview.h
@@ -92,39 +92,89 @@ Scatter Phase Kernels::
 
 \subsection data_structures MPGraph Data Structures
 
-In order to write code to the MPGraph API, you need to be aware of the
-following data structures:
+In order to write code to the MPGraph API, it helps to have a
+high-level understanding of the data structures used to maintain the
+frontier, the topology, and the user data associated with the vertices
+and edges of the graph.
 
-- Frontier: The frontier is a dynamic queue containing those vertices
-  that are active in each iteration.  The frontier is managed by the
-  MPGraph kernels, but user data may be allocated and accessed that is
-  1:1 with the frontier.  For example, BFS uses a scratch array to
-  store the predecessor value.
+\subsubsection frontier Frontier Queues
 
-- Topology: The topology is built from the sparse matrix data file. It
-  is currently maintained in a CSR (Compressed Sparse Row) and/or CSC
-  (Compressed Sparse Column) data structures, depending on the access
-  patterns required by the Gather and/or Scatter phases.  Users do not
-  have direct access to these data structures and the implementations
-  are likely to evolve substantially, e.g., to support topology
-  compression and decomposition of large graphs and MPGraph algorithms
-  on GPU workstations and GPU clusters.
+MPGraph uses frontier queues to maintain a dense list of the active
+vertices.  These queues are managed by the MPGraph kernels, but user
+data may be allocated and accessed that is 1:1 with the frontier (see
+below).  The frontier array dimensions are determined by the number of
+vertices in the graph times the frontier queue size multiplier. The
+frontier is in global memory, but is buffered in shared memory by some
+kernels.
 
-- Vertex list: The vertex list is a structure of arrays pattern.  See
-  a VertexData structure in one of the existing algorithms for
-  examples.
+\image html MPGraph-Frontier.jpg "MPGraph Frontier Queues"
 
-- Edge list: The edge list is a structure of arrays pattern. See an
-  EdgeData structure in one of the existing algorithms for examples.
+The frontier is populated by the expand() kernel.  The contract()
+kernel may be used to eliminate some or all of the duplicates
+depending on the strategy and the needs of the graph algorithm. There
+are actually two two frontier queues - this is for double-buffering.
 
-The user data (vertex list and edge list) are laid out in vertical
-stripes using a Structures of Arrays pattern for optimal memory access
-patterns on the GPU.  To add your own data, you add a field to the
-vertex data struct or the edge data struct. That field will be an
-array that is 1:1 with the vertex identifiers.  You will need to
-initialize your array.  MPGraph will provide you with access to your
-data from within the appropriate device functions.
+In addition to the frontier, you may allocate optional user data
+arrays that are 1:1 with the active vertices in the frontier.  These
+arrays provides an important scratch area for many calculations and
+benefit from dense, coalesced access. The arrays are accessed from the
+same kernels that operate on the vertex frontier.  For example, BFS
+uses a scratch array to store the predecessor value.
 
+\subsubsection frontier Graph Topology
+
+The topology of the graph is modeled by a forward and reverse sparse
+matrix and is constructed at runtime from the sparse matrix data file.
+The digrams below illustrate the use of CSR and CSC data structures to
+model the graph topology. However, these topology data structures are
+not directly exposed to user algorithms and their internals may
+change.  Users write device functions that are invoked from kernels
+that process the topology using a variety of different strategies.
+Users do not need to access or understand the internals of the
+topology data structures to write graph algorithms.
+
+\image html MPGraph-Topology.jpg "MPGraph Topology"
+
+The forward topology index is currently a Compressed Sparse Row (CSR)
+matrix that provides row based indexing into the graph.  This data
+structure is not directly exposed to user algorithms.  CSR is used to
+access (traverse) the out-edges of the graph.
+
+The reverse topology index is currently a Compressed Sparse Column
+(CSC) matrix that provides column based indexing into the graph.  This
+data structure is not directly exposed to user algorithms.  CSC is
+used to access (traverse) the in-edges of the graph. The CSC edgeId
+array gives the index into the EdgeList arrays. The CSC data structure
+is only maintained if the algorithm will read over the in-edges.
+
+\subsubsection user-data User Data
+
+User data is specific to a given algorithm.  It is laid out in a
+structure of arrays format in order to maximize coalesced memory
+access.  (User data can also be 1:1 with the frontier - see above.)
+There are two basic user data structures: The vertex list and the edge
+list.
+
+\image html MPGraph-UserData.jpg "MPGraph User Data"
+
+The VertexList is a structure of named arrays that provides data for
+each vertex in the graph.  The index into each array is the vertexId.
+See the VertexData structure in one of the existing algorithms for
+examples.
+
+The EdgeList is a structure of named arrays that provides data for
+each edge in the graph.  The index into each array is the edgeId. The
+CSR.colind[] and the CSC.edgeId[] are both 1:1 with the edge list
+arrays. Again, see an EdgeData structure in one of the existing
+algorithms for examples.
+
+The vertex list and edge list are laid out in vertical stripes using a
+Structures of Arrays pattern for optimal memory access patterns on the
+GPU.  To add your own data, you add a field to the vertex data struct
+or the edge data struct. That field will be an array that is 1:1 with
+the vertex identifiers.  You will need to initialize your array.
+MPGraph will provide you with access to your data from within the
+appropriate device functions.
 
 \subsection write_your_own Writing your own MPGraph algorithm
 
