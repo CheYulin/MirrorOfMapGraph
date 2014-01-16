453a3512e4a973a2d5011905e5c1d4f751397d31
Bryan Thompson <bryan@systap.com>
2014-01-16 14:03:20 +0000

    added largePerformanceGraphs directory with makefiles but not the data. moved top-level batch scripts into the CI directory to work on CI setup. modifying CI/proc.sh to work with new command line.

--- CI/proc.sh
+++ CI/proc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Directory containing the mpgraph executables.
-BIN_DIR=..
+BIN_DIR=../Algorithms
 # Directory containing the graph datasets to be processed.
 DATA_DIR=/data/xdata/GraphDatasets
 # Directory containing the matching files of random vertices for the initial frontier.
@@ -14,7 +14,8 @@ TYPES="SSSP"; # BFS SSSP PR CC BC
 ##
 
 # Symmetric matrices.
-SYM_DATA="ak2010 belgium_osm coAuthorsDBLP delaunay_n13 delaunay_n21 kron_g500-logn20 bitcoin"
+#SYM_DATA="ak2010 belgium_osm coAuthorsDBLP delaunay_n13 delaunay_n21 kron_g500-logn20 bitcoin"
+SYM_DATA="ak2010"
 
 # Generalized (aka non-symmetric) matrices.
 #GEN_DATA="webbase-1M wikipedia-20070206 twitter_d1";# traceroute"
@@ -31,23 +32,34 @@ N=1
 
 #### end changable section
 
+nerr=0
 for TYPE in $TYPES
   do
   for graph in $DATA
     do 
           # The name of the executable.
-    exec="$BIN_DIR/simple$TYPE"
+    exec="$BIN_DIR/$TYPE/$TYPE"
           # remove old log file for this graph.
     rm -f out.$TYPE.$graph.log
 	    # 1 iff this is an undirected graph (aka symmetric). 0 otherwise.
-    export undirectedGraph=`echo $SGEN_DATA | grep -c "$graph"`
+    export directedGraph=`echo $SYM_DATA | grep -c "$graph"`
     export vertices=`head -n $N "$RAND_DIR/$graph.rand_indices"`
 	    #echo "graph=[$graph], directedGraph=[$directedGraph]"
     for src in ${vertices}
       do
-      cmd="$exec $DATA_DIR/$graph$suffix foo $src $undirectedGraph"
+      cmd="$exec -g $DATA_DIR/$graph$suffix foo -p src=$src directed=$directedGraph"
       echo "Running: $cmd"
       $cmd >> out.$TYPE.$graph.log 2>&1
+      if [[ $rc != 0 ]] ; then
+	  echo "Error: $cmd"
+	  nerr=`expr $nerr + 1`
+      fi
     done
   done
 done
+if [[ $nerr != 0 ]] ; then
+    echo "There were $nerr errors."
+    exit 1
+fi
+echo "Success."
+exit 0
