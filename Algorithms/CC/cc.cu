ed7171f2a2c141fb6c54ca2d00128f8379af2146
Zhisong Fu <fuzhisong@systap.com>
2014-04-21 21:06:59 +0000

    Add or correct copyright headers for MULTI_GPU branch

--- Algorithms/CC/cc.cu
+++ Algorithms/CC/cc.cu
@@ -1,19 +1,24 @@
-/*
- Copyright (C) SYSTAP, LLC 2006-2014.  All rights reserved.
+/**
+Copyright 2013-2014 SYSTAP, LLC.  http://www.systap.com
 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
- http://www.apache.org/licenses/LICENSE-2.0
+   http://www.apache.org/licenses/LICENSE-2.0
 
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
+This work was (partially) funded by the DARPA XDATA program under
+AFRL Contract #FA8750-13-C-0002.
+
+This material is based upon work supported by the Defense Advanced
+Research Projects Agency (DARPA) under Contract No. D14PC00029.
+*/
 typedef unsigned int uint;
 #include <stdio.h>
 #include <cstdlib>
