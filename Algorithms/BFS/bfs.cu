620e7ecb4d3ad1d41cec4a23da9e1c85ae8b9223
Zhisong Fu <fuzhisong@systap.com>
2014-10-17 21:25:02 +0000

    compute predecessor list on GPU

--- Algorithms/BFS/bfs.cu
+++ Algorithms/BFS/bfs.cu
@@ -35,6 +35,7 @@ typedef unsigned int uint;
 
 #include <config.h>
 
+
 // Utilities and correctness-checking
 //#include <test/b40c_test_util.h>
 
@@ -46,23 +47,25 @@ typedef unsigned int uint;
 
 #include <GASengine/csr_problem.cuh>
 #include <GASengine/enactor_vertex_centric.cuh>
+#include <predextract.cuh>
 
 using namespace b40c;
 using namespace graph;
 using namespace std;
 
 template<
-    typename VertexId,
-    typename Value,
-    typename SizeT>
+typename VertexId,
+typename Value,
+typename SizeT>
 void CPUBFS(
-    int directed,
-    int test_iteration,
-    const CsrGraph<VertexId, Value, SizeT> &csr_graph,
-    VertexId *source_path,
-    VertexId src)
+            int directed,
+            int test_iteration,
+            const CsrGraph<VertexId, Value, SizeT> &csr_graph,
+            VertexId *source_path,
+            VertexId src)
 {
-  printf("Running CPU code...");fflush(stdout);
+  printf("Running CPU code...");
+  fflush(stdout);
 
   // (Re)initialize distances
   for (VertexId i = 0; i < csr_graph.nodes; i++)
@@ -109,25 +112,25 @@ void CPUBFS(
       }
     }
 
-    if(directed==0)
+    if (directed == 0)
     {
-       edges_begin = csr_graph.column_offsets[dequeued_node];
-       edges_end = csr_graph.column_offsets[dequeued_node + 1];
-       for (int edge = edges_begin; edge < edges_end; edge++)
-       {
+      edges_begin = csr_graph.column_offsets[dequeued_node];
+      edges_end = csr_graph.column_offsets[dequeued_node + 1];
+      for (int edge = edges_begin; edge < edges_end; edge++)
+      {
 
         // Lookup neighbor and enqueue if undiscovered
-         VertexId neighbor = csr_graph.row_indices[edge];
-         if (source_path[neighbor] == -1)
-         {
-            source_path[neighbor] = neighbor_dist;
-            if (search_depth < neighbor_dist)
-            {
-              search_depth = neighbor_dist;
-            }
-            frontier.push_back(neighbor);
+        VertexId neighbor = csr_graph.row_indices[edge];
+        if (source_path[neighbor] == -1)
+        {
+          source_path[neighbor] = neighbor_dist;
+          if (search_depth < neighbor_dist)
+          {
+            search_depth = neighbor_dist;
           }
-       }
+          frontier.push_back(neighbor);
+        }
+      }
     }
     iter++;
   }
@@ -135,7 +138,7 @@ void CPUBFS(
   const time_t EndTime = time(NULL);
 
   std::cout << "CPU time took: " << difftime(EndTime, startTime) * 1000 << " ms"
-      << std::endl;
+          << std::endl;
   search_depth++;
 }
 
@@ -146,10 +149,10 @@ bool cudaInit(int device)
 
   if (error_id != cudaSuccess)
   {
-    printf("cudaGetDeviceCount returned %d\n-> %s\n", (int) error_id,
-        cudaGetErrorString(error_id));
+    printf("cudaGetDeviceCount returned %d\n-> %s\n", (int)error_id,
+           cudaGetErrorString(error_id));
     printf("Result = FAIL\n");
-    exit (EXIT_FAILURE);
+    exit(EXIT_FAILURE);
   }
 
   // This function call returns 0 if there are no CUDA capable devices.
@@ -180,16 +183,16 @@ bool cudaInit(int device)
       cudaDriverGetVersion(&driverVersion);
       cudaRuntimeGetVersion(&runtimeVersion);
       printf(
-          "  CUDA Driver Version / Runtime Version          %d.%d / %d.%d\n",
-          driverVersion / 1000, (driverVersion % 100) / 10,
-          runtimeVersion / 1000, (runtimeVersion % 100) / 10);
+             "  CUDA Driver Version / Runtime Version          %d.%d / %d.%d\n",
+             driverVersion / 1000, (driverVersion % 100) / 10,
+             runtimeVersion / 1000, (runtimeVersion % 100) / 10);
       printf("  CUDA Capability Major/Minor version number:    %d.%d\n",
-          deviceProp.major, deviceProp.minor);
+             deviceProp.major, deviceProp.minor);
 
       printf(
-          "  Total amount of global memory:                 %.0f MBytes (%llu bytes)\n",
-          (float) deviceProp.totalGlobalMem / 1048576.0f,
-          (unsigned long long) deviceProp.totalGlobalMem);
+             "  Total amount of global memory:                 %.0f MBytes (%llu bytes)\n",
+             (float)deviceProp.totalGlobalMem / 1048576.0f,
+             (unsigned long long)deviceProp.totalGlobalMem);
 
       break;
     }
@@ -201,12 +204,13 @@ bool cudaInit(int device)
 bool correctTest(int nodes, int* reference_labels, int* h_labels)
 {
   bool pass = true;
-  printf("Correctness testing ...");fflush(stdout);
+  printf("Correctness testing ...");
+  fflush(stdout);
   for (int i = 0; i < nodes; i++)
   {
     if (reference_labels[i] != h_labels[i])
     {
-//      printf("Incorrect value for node %d: CPU value %d, GPU value %d\n", i, reference_labels[i], h_labels[i]);
+      //      printf("Incorrect value for node %d: CPU value %d, GPU value %d\n", i, reference_labels[i], h_labels[i]);
       pass = false;
     }
   }
@@ -220,15 +224,15 @@ bool correctTest(int nodes, int* reference_labels, int* h_labels)
 void printUsageAndExit(char *algo_name)
 {
   std::cout
-      << "Usage: " << algo_name
-      << " [-graph (-g) graph_file] [-output (-o) output_file] [-sources src_file] [-BFS \"variable1=value1 variable2=value2 ... variable3=value3\" -help ] [-c config_file]\n";
+          << "Usage: " << algo_name
+          << " [-graph (-g) graph_file] [-output (-o) output_file] [-sources src_file] [-BFS \"variable1=value1 variable2=value2 ... variable3=value3\" -help ] [-c config_file]\n";
   std::cout << "     -help display the command options\n";
   std::cout << "     -graph specify a sparse matrix in Matrix Market (.mtx) format\n";
   std::cout << "     -output or -o specify file for output result\n";
   std::cout << "     -sources or -s set starting vertices file\n";
   std::cout << "     -c set the BFS options from the configuration file\n";
   std::cout
-      << "     -parameters (-p) set the options.  Options include the following:\n";
+          << "     -parameters (-p) set the options.  Options include the following:\n";
   Config::printOptions();
 
   exit(0);
@@ -238,8 +242,8 @@ int main(int argc, char **argv)
 {
 
   const char* outFileName = 0;
-//  int src[1];
-//  bool g_undirected;
+  //  int src[1];
+  //  bool g_undirected;
   const bool g_stream_from_host = false;
   const bool g_with_value = true;
   const bool g_mark_predecessor = false;
@@ -250,8 +254,8 @@ int main(int argc, char **argv)
   char* graph_file = NULL;
   CsrGraph<VertexId, Value, SizeT> csr_graph(g_stream_from_host);
   char source_file_name[100] = "";
-//  int device = 0;
-//  double max_queue_sizing = 1.3;
+  //  int device = 0;
+  //  double max_queue_sizing = 1.3;
   Config cfg;
 
   for (int i = 1; i < argc; i++)
@@ -259,7 +263,7 @@ int main(int argc, char **argv)
     if (strncmp(argv[i], "-help", 100) == 0) // print the usage information
       printUsageAndExit(argv[0]);
     else if (strncmp(argv[i], "-graph", 100) == 0
-        || strncmp(argv[i], "-g", 100) == 0)
+             || strncmp(argv[i], "-g", 100) == 0)
     { //input graph
       i++;
 
@@ -296,7 +300,8 @@ int main(int argc, char **argv)
     exit(1);
   }
 
-  bool cudaEnabled = cudaInit(cfg.getParameter<int>("device"));
+  int device_id = cfg.getParameter<int>("device");
+  bool cudaEnabled = cudaInit(device_id);
   if (!cudaEnabled)
     return 0;
 
@@ -309,21 +314,21 @@ int main(int argc, char **argv)
 
   int directed = cfg.getParameter<int>("directed");
 
-/*
-  if(strcmp(graph_file, "RMAT") == 0)
+  /*
+    if(strcmp(graph_file, "RMAT") == 0)
+    {
+      int tmpn = pow(2,21);
+      int tmpe = tmpn * 16;
+      if(builder::BuildRmatGraph<g_with_value>(tmpn, tmpe, csr_graph, true, 0.45, 0.15, 0.15) != 0)
+        exit(1);
+    }
+    else*/
   {
-    int tmpn = pow(2,21);
-    int tmpe = tmpn * 16;
-    if(builder::BuildRmatGraph<g_with_value>(tmpn, tmpe, csr_graph, true, 0.45, 0.15, 0.15) != 0)
+    if (builder::BuildMarketGraph<g_with_value > (graph_file, csr_graph,
+                                                  false) != 0)
       exit(1);
   }
-  else*/
-  {
-  if (builder::BuildMarketGraph<g_with_value>(graph_file, csr_graph,
-      false) != 0)
-    exit(1);
-  }
-//  csr_graph.DisplayGraph();
+  //  csr_graph.DisplayGraph();
   int num_srcs = 0;
   int* srcs = NULL;
   int origin = cfg.getParameter<int>("origin");
@@ -340,7 +345,8 @@ int main(int argc, char **argv)
       num_srcs = cfg.getParameter<int>("num_src");
       srcs = new int[num_srcs];
       printf("Using %d random starting vertices!\n", num_srcs);
-      srand (time(NULL));int
+      srand(time(NULL));
+      int
       count = 0;
       while (count < num_srcs)
       {
@@ -391,10 +397,11 @@ int main(int argc, char **argv)
   }
 
   {
-	  const int stats = cfg.getParameter<int>("stats");
-	  if(stats) {
-		  csr_graph.PrintHistogram();
-	  }
+    const int stats = cfg.getParameter<int>("stats");
+    if (stats)
+    {
+      csr_graph.PrintHistogram();
+    }
   }
 
   VertexId* reference_labels;
@@ -402,7 +409,7 @@ int main(int argc, char **argv)
   int run_CPU = cfg.getParameter<int>("run_CPU");
   if (strcmp(source_file_name, "") == 0 && run_CPU) //Do correctness test only with single starting vertex
   {
-    reference_labels = (VertexId*) malloc(sizeof(VertexId) * csr_graph.nodes);
+    reference_labels = (VertexId*)malloc(sizeof (VertexId) * csr_graph.nodes);
     int test_iteration = 1;
     int src = cfg.getParameter<int>("src");
 
@@ -410,22 +417,22 @@ int main(int argc, char **argv)
       src--;
 
     CPUBFS(
-        directed,
-        test_iteration,
-        csr_graph,
-        reference_labels,
-        src);
+           directed,
+           test_iteration,
+           csr_graph,
+           reference_labels,
+           src);
   }
 
-// Allocate problem on GPU
+  // Allocate problem on GPU
   int num_gpus = 1;
   typedef GASengine::CsrProblem<bfs, VertexId, SizeT, Value, g_mark_predecessor, g_with_value> CsrProblem;
   CsrProblem csr_problem(cfg);
 
   if (csr_problem.FromHostProblem(g_stream_from_host, csr_graph.nodes,
-      csr_graph.edges, csr_graph.column_indices,
-      csr_graph.row_offsets, csr_graph.edge_values, csr_graph.row_indices,
-      csr_graph.column_offsets, num_gpus, directed))
+                                  csr_graph.edges, csr_graph.column_indices,
+                                  csr_graph.row_offsets, csr_graph.edge_values, csr_graph.row_indices,
+                                  csr_graph.column_offsets, num_gpus, directed))
     exit(1);
 
   const bool INSTRUMENT = true;
@@ -448,29 +455,46 @@ int main(int argc, char **argv)
     }
   }
 
-  Value* h_values = (Value*) malloc(sizeof(Value) * csr_graph.nodes);
-  csr_problem.ExtractResults(h_values);
+  int* d_predlist;
+  cudaMalloc((void**)&d_predlist, csr_graph.nodes * sizeof (int));
   
+  //extract the predecessor list: the predecessor computed for the src vertex is arbitrary and should be ignored. 
+  predextract<bfs, VertexId, SizeT, Value, g_mark_predecessor, g_with_value > (csr_problem, device_id, d_predlist);
+
+//  int* test_vid = new int[csr_graph.nodes];
+//  cudaMemcpy(test_vid, d_predlist, csr_graph.nodes * sizeof (int), cudaMemcpyDeviceToHost);
+//  printf("d_predlist: ");
+//  for (int i = 0; i < (csr_graph.nodes); ++i)
+//  {
+//    printf("%d, ", test_vid[i]);
+//  }
+//  printf("\n");
+//  delete[] test_vid;
+
+  Value* h_values = (Value*)malloc(sizeof (Value) * csr_graph.nodes);
+  csr_problem.ExtractResults(h_values);
+
   //compute GTEPS
   long long te = 0;
-  for(int i=0; i<csr_graph.nodes; i++)
+  for (int i = 0; i < csr_graph.nodes; i++)
   {
-    if(h_values[i] != -1)
+    if (h_values[i] != -1)
     {
-      te += csr_graph.row_offsets[i+1] -  csr_graph.row_offsets[i];
+      te += csr_graph.row_offsets[i + 1] - csr_graph.row_offsets[i];
     }
   }
-  
+
   printf("Traversed edge: %lld\n", te);
 
   if (strcmp(source_file_name, "") == 0 && run_CPU)
   {
     const bool ok = correctTest(csr_graph.nodes, reference_labels, h_values);
     free(reference_labels);
-	if (!ok) {
-		fprintf(stderr, "correctness test failed.");
-		exit(1);
-	}
+    if (!ok)
+    {
+      fprintf(stderr, "correctness test failed.");
+      exit(1);
+    }
   }
 
   if (outFileName)
